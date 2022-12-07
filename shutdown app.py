96173eddbccda3aba89e5034735dd55e2898e823
Wasif Ansari <wasifansari2709@gmail.com>
2022-12-07 21:35:14 +0530

    creating restart and restart with time button

--- shutdown app.py
+++ shutdown app.py
@@ -7,5 +7,14 @@ st.geometry("500x500")
 st.config(bg="blue")
 
 
+r_button = Button(st,text = "Restart", font=("Time New Roman",25,"bold"),
+relief = RAISED,cursor = "plus")
+r_button.place(x=150,y=60,height = 50,width=200)
+
+rt_button = Button(st,text = "Restart Time", font=("Time New Roman",25,"bold"),
+relief = RAISED,cursor = "plus")
+rt_button.place(x=150,y=170,height = 50,width=200)
+
+
 
 st.mainloop()
